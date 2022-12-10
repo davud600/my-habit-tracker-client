9eeef730249bceab75920cb91c57748acba35fb8
davud <davudgorqi@gmail.com>
2022-12-10 11:49:51 +0100

    pie area chart styling

--- src/components/GraphSection.jsx
+++ src/components/GraphSection.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useHabits } from "../Habits";
-import { Line } from "react-chartjs-2";
+import { Line, PolarArea } from "react-chartjs-2";
 import {
     Chart as ChartJS,
     CategoryScale,
     LinearScale,
     PointElement,
     LineElement,
+    RadialLinearScale,
+    ArcElement,
     Title,
     Tooltip,
     Filler,
     Legend
 } from "chart.js";
 
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -101,7 +104,7 @@ const options = {
         },
         title: {
             display: true,
-            text: "Habits Chart"
+            text: "Habits Time Graph"
         }
     },
     scales: {
@@ -118,8 +121,39 @@ const options = {
     }
 };
 
+const polarAreaOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+            position: "top"
+        },
+        title: {
+            display: true,
+            text: "Habits Frequency Rate %"
+        }
+    },
+    scales: {
+        r: {
+            ticks: {
+                display: false
+            },
+            grid: {
+                color: "rgba(180, 180, 180, 0.2)"
+            },
+            pointLabels: {
+                display: true,
+                centerPointLabels: true,
+                font: {
+                    size: 17
+                }
+            }
+        }
+    }
+};
+
 export default function GraphSection() {
-    const { fetchedRegisteredHabits } = useHabits();
+    const { fetchedRegisteredHabits, habits } = useHabits();
 
     let prevMonth;
     const Data = dates.map(date => {
@@ -164,38 +198,81 @@ export default function GraphSection() {
         };
     });
 
-    const [chartData, setChartData] = useState({
+    const habitsRates = habits.map(habit => {
+        // Count how many times this habit has been registered in total
+        // Calculate completion rate = total / days
+        let habitTimesRegistered = 0;
+        let totalDays = dates.length;
+        fetchedRegisteredHabits.forEach(registeredHabit => {
+            if (registeredHabit.habit_id === habit.id) habitTimesRegistered++;
+        });
+        const completionRate = Math.round(
+            (habitTimesRegistered / totalDays) * 100
+        );
+
+        return completionRate;
+    });
+
+    const chartDataInitialState = {
         labels: Data.map(data => data.date),
         datasets: [
             {
                 fill: true,
                 label: "Habits Completed",
                 data: Data.map(data => data.numOfHabitsCompleted),
-                backgroundColor: "rgba(255, 50, 50, 0.3)",
-                borderColor: "rgba(255, 35, 35, 0.6)",
+                backgroundColor: "rgba(255, 50, 50, 0.25)",
+                borderColor: "rgba(200, 35, 35, 0.5)",
                 borderWidth: 3
             }
         ]
-    });
+    };
+    const polarAreaDataInitialState = {
+        labels: habits.map(habit => habit.name),
+        datasets: [
+            {
+                fill: true,
+                label: "Habit Frequency",
+                data: habitsRates,
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.5)",
+                    "rgba(54, 162, 235, 0.5)",
+                    "rgba(255, 206, 86, 0.5)",
+                    "rgba(75, 192, 192, 0.5)",
+                    "rgba(153, 102, 255, 0.5)",
+                    "rgba(255, 159, 64, 0.5)"
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 0.75)",
+                    "rgba(54, 162, 235, 0.75)",
+                    "rgba(255, 206, 86, 0.75)",
+                    "rgba(75, 192, 192, 0.75)",
+                    "rgba(153, 102, 255, 0.75)",
+                    "rgba(255, 159, 64, 0.75)"
+                ],
+                borderWidth: 2
+            }
+        ]
+    };
+
+    const [chartData, setChartData] = useState(chartDataInitialState);
+    const [polarAreaData, setPolarAreaData] = useState(
+        polarAreaDataInitialState
+    );
 
     useEffect(() => {
-        setChartData({
-            labels: Data.map(data => data.date),
-            datasets: [
-                {
-                    fill: true,
-                    label: "Habits Completed",
-                    data: Data.map(data => data.numOfHabitsCompleted),
-                    backgroundColor: "rgba(255, 50, 50, 0.3)",
-                    borderColor: "rgba(255, 35, 35, 0.6)",
-                    borderWidth: 3
-                }
-            ]
-        });
+        setChartData(chartDataInitialState);
+        setPolarAreaData(polarAreaDataInitialState);
     }, [fetchedRegisteredHabits]);
 
     return (
         <section className='graph-section'>
+            <div className='polar-area-chart-container mb-12 mt-5'>
+                <PolarArea
+                    className='polar-area-chart'
+                    options={polarAreaOptions}
+                    data={polarAreaData}
+                />
+            </div>
             <div className='graph-container'>
                 <Line
                     className='line-chart'
