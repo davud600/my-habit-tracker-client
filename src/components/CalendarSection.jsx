1f3ea2b4741b53c4146b42134084a3d8c2ff28ee
davud <davudgorqi@gmail.com>
2023-01-01 23:29:49 +0100

    fixed bug where calendar would break in different years

--- src/components/CalendarSection.jsx
+++ src/components/CalendarSection.jsx
@@ -6,67 +6,71 @@ const currYear = 2022;
 const dateObj = new Date();
 const dates = [];
 
-monthsLoop: for (let m = 11; m <= 12; m++) {
-    let monthDays;
+yearsLoop: for (let y = 2022; y < 2024; y++) {
+    const initMonth = y === 2022 ? 11 : 1;
 
-    switch (m) {
-        case 1:
-            monthDays = 31;
-            break;
-        case 2:
-            monthDays = new Date(currYear, 1, 29).getDate() === 29 ? 29 : 28;
-            break;
-        case 3:
-            monthDays = 31;
-            break;
-        case 4:
-            monthDays = 30;
-            break;
-        case 5:
-            monthDays = 31;
-            break;
-        case 6:
-            monthDays = 30;
-            break;
-        case 7:
-            monthDays = 31;
-            break;
-        case 8:
-            monthDays = 31;
-            break;
-        case 9:
-            monthDays = 30;
-            break;
-        case 10:
-            monthDays = 31;
-            break;
-        case 11:
-            monthDays = 30;
-            break;
-        case 12:
-            monthDays = 31;
-            break;
-        default:
-            break;
-    }
+    for (let m = initMonth; m <= 12; m++) {
+        let monthDays;
 
-    // This is because we started at nov 16
-    let initialD = 1;
-    if (m === 11) initialD = 16;
+        switch (m) {
+            case 1:
+                monthDays = 31;
+                break;
+            case 2:
+                monthDays =
+                    new Date(currYear, 1, 29).getDate() === 29 ? 29 : 28;
+                break;
+            case 3:
+                monthDays = 31;
+                break;
+            case 4:
+                monthDays = 30;
+                break;
+            case 5:
+                monthDays = 31;
+                break;
+            case 6:
+                monthDays = 30;
+                break;
+            case 7:
+                monthDays = 31;
+                break;
+            case 8:
+                monthDays = 31;
+                break;
+            case 9:
+                monthDays = 30;
+                break;
+            case 10:
+                monthDays = 31;
+                break;
+            case 11:
+                monthDays = 30;
+                break;
+            case 12:
+                monthDays = 31;
+                break;
+            default:
+                break;
+        }
 
-    for (let d = initialD; d <= monthDays; d++) {
-        let isToday =
-            dateObj.getDate() === d &&
-            dateObj.getFullYear() === currYear &&
-            dateObj.getMonth() + 1 === m;
+        // This is because we started at nov 16 2022
+        const initialD = m === 11 && y === 2022 ? 16 : 1;
 
-        dates.push({
-            day: d,
-            month: m,
-            year: currYear
-        });
+        for (let d = initialD; d <= monthDays; d++) {
+            let isToday =
+                dateObj.getDate() === d &&
+                dateObj.getFullYear() === y &&
+                dateObj.getMonth() + 1 === m;
 
-        if (isToday) break monthsLoop;
+            dates.push({
+                day: d,
+                month: m,
+                year: y
+            });
+
+            if (isToday) break yearsLoop;
+        }
     }
 }
 
