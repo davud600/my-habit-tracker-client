f4b56db2cba4da6f898c64782385b962c316b0cc
davud <davudgorqi@gmail.com>
2023-01-01 23:36:26 +0100

    exported function from new file more organized

--- src/components/CalendarSection.jsx
+++ src/components/CalendarSection.jsx
@@ -1,78 +1,7 @@
 import React from "react";
 import { useHabits } from "../Habits";
 import DayCard from "./DayCard";
-
-const currYear = 2022;
-const dateObj = new Date();
-const dates = [];
-
-yearsLoop: for (let y = 2022; y < 2024; y++) {
-    const initMonth = y === 2022 ? 11 : 1;
-
-    for (let m = initMonth; m <= 12; m++) {
-        let monthDays;
-
-        switch (m) {
-            case 1:
-                monthDays = 31;
-                break;
-            case 2:
-                monthDays =
-                    new Date(currYear, 1, 29).getDate() === 29 ? 29 : 28;
-                break;
-            case 3:
-                monthDays = 31;
-                break;
-            case 4:
-                monthDays = 30;
-                break;
-            case 5:
-                monthDays = 31;
-                break;
-            case 6:
-                monthDays = 30;
-                break;
-            case 7:
-                monthDays = 31;
-                break;
-            case 8:
-                monthDays = 31;
-                break;
-            case 9:
-                monthDays = 30;
-                break;
-            case 10:
-                monthDays = 31;
-                break;
-            case 11:
-                monthDays = 30;
-                break;
-            case 12:
-                monthDays = 31;
-                break;
-            default:
-                break;
-        }
-
-        // This is because we started at nov 16 2022
-        const initialD = m === 11 && y === 2022 ? 16 : 1;
-
-        for (let d = initialD; d <= monthDays; d++) {
-            let isToday =
-                dateObj.getDate() === d &&
-                dateObj.getFullYear() === y &&
-                dateObj.getMonth() + 1 === m;
-
-            dates.push({
-                day: d,
-                month: m,
-                year: y
-            });
-
-            if (isToday) break yearsLoop;
-        }
-    }
-}
+import { dates, dateObj } from "../dates";
 
 export default function CalendarSection() {
     const { fetchedRegisteredHabits } = useHabits();
--- src/components/GraphSection.jsx
+++ src/components/GraphSection.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHabits } from "../Habits";
 import { Line, PolarArea } from "react-chartjs-2";
+import { dates } from "../dates";
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -26,74 +27,6 @@ ChartJS.register(
     Filler,
     Legend
 );
-
-const currYear = 2022;
-const dateObj = new Date();
-const dates = [];
-
-monthsLoop: for (let m = 11; m <= 12; m++) {
-    let monthDays;
-
-    switch (m) {
-        case 1:
-            monthDays = 31;
-            break;
-        case 2:
-            monthDays = new Date(currYear, 1, 29).getDate() === 29 ? 29 : 28;
-            break;
-        case 3:
-            monthDays = 31;
-            break;
-        case 4:
-            monthDays = 30;
-            break;
-        case 5:
-            monthDays = 31;
-            break;
-        case 6:
-            monthDays = 30;
-            break;
-        case 7:
-            monthDays = 31;
-            break;
-        case 8:
-            monthDays = 31;
-            break;
-        case 9:
-            monthDays = 30;
-            break;
-        case 10:
-            monthDays = 31;
-            break;
-        case 11:
-            monthDays = 30;
-            break;
-        case 12:
-            monthDays = 31;
-            break;
-        default:
-            break;
-    }
-
-    // This is because we started at nov 16
-    let initialD = 1;
-    if (m === 11) initialD = 16;
-
-    for (let d = initialD; d <= monthDays; d++) {
-        let isToday =
-            dateObj.getDate() === d &&
-            dateObj.getFullYear() === currYear &&
-            dateObj.getMonth() + 1 === m;
-
-        dates.push({
-            day: d,
-            month: m,
-            year: currYear
-        });
-
-        if (isToday) break monthsLoop;
-    }
-}
 
 const options = {
     responsive: true,
